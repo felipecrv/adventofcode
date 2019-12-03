14166c2e08e8acc8cb209a81cdf33d903066e2cc
Felipe Oliveira Carvalho <felipekde@gmail.com>
2019-12-03 18:46:46 +0100

    Day 02

---
+++ 2019/02/prog_gold.cpp
@@ -0,0 +1,92 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <string>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+const int MAX_MEM = 1000;
+
+int prog[MAX_MEM];
+int mem[MAX_MEM];
+
+int run(int *mem, int len, int noun, int verb) {
+  mem[1] = noun;
+  mem[2] = verb;
+  bool did_halt = false;
+
+  int pc = 0;
+  while (pc < len) {
+    int opcode = mem[pc];
+    switch (opcode) {
+      case 1:
+      case 2: {
+        int addr_a = mem[pc + 1];
+        int addr_b = mem[pc + 2];
+        int addr_dest = mem[pc + 3];
+        assert(addr_a < len);
+        assert(addr_b < len);
+        assert(addr_dest < len);
+        int a = mem[addr_a];
+        int b = mem[addr_b];
+        mem[addr_dest] = (opcode == 1) ? a + b : a * b;
+        break;
+      }
+      case 99:
+        goto halt;
+        break;
+    }
+    pc += 4;
+    continue;
+  halt:
+    did_halt = true;
+    break;
+  }
+
+  assert(did_halt);
+  return mem[0];
+}
+
+int main() {
+  memset(prog, 0, sizeof(prog));
+  memset(mem, 0, sizeof(mem));
+
+  int len = 0;
+  while (scanf("%d", &prog[len++])) {
+    char c = getchar();
+    if (c == EOF || c == '\n') {
+      break;
+    } else {
+      assert(c == ',');
+    }
+  }
+
+  for (int noun = 0; noun < 100; noun++) {
+    for (int verb = 0; verb < 100; verb++) {
+      memcpy(mem, prog, MAX_MEM * sizeof(mem[0]));
+      int result = run(mem, len, noun, verb);
+      if (result == 19690720) {
+        printf("%d %d -> %d\n", noun, verb, result);
+        break;  
+      }
+    }
+  }
+
+  for (int i = 0; i < len; i++) {
+    printf("%d,", mem[i]);
+  }
+  putchar('\n');
+
+  return 0;
+}
---
+++ 2019/02/prog_silver.cpp
@@ -0,0 +1,75 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <string>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+const int MAX_MEM = 1000000;
+
+int prog[MAX_MEM];
+
+int main() {
+  memset(prog, 0, sizeof(prog));
+
+  int len = 0;
+  while (scanf("%d", &prog[len++])) {
+    char c = getchar();
+    if (c == EOF || c == '\n') {
+      break;
+    } else {
+      assert(c == ',');
+    }
+  }
+
+  prog[1] = 12;
+  prog[2] = 2;
+  bool did_halt = false;
+
+  int pc = 0;
+  while (pc < len) {
+    int opcode = prog[pc];
+    switch (opcode) {
+      case 1:
+      case 2: {
+        int addr_a = prog[pc + 1];
+        int addr_b = prog[pc + 2];
+        int addr_dest = prog[pc + 3];
+        assert(addr_a < len);
+        assert(addr_b < len);
+        assert(addr_dest < MAX_MEM);
+        int a = prog[addr_a];
+        int b = prog[addr_b];
+        prog[addr_dest] = (opcode == 1) ? a + b : a * b;
+        break;
+      }
+      case 99:
+        goto halt;
+        break;
+    }
+    pc += 4;
+    continue;
+  halt:
+    did_halt = true;
+    break;
+  }
+
+  assert(did_halt);
+  for (int i = 0; i < len; i++) {
+    printf("%d,", prog[i]);
+  }
+  putchar('\n');
+
+  return 0;
+}

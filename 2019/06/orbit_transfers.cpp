b260ddba718c03ca23f04943a39dec0961e80dad
Felipe Oliveira Carvalho <felipekde@gmail.com>
2019-12-06 09:11:21 +0100

    Simplify day 6 solution

--- 2019/06/orbit_transfers.cpp
+++ 2019/06/orbit_transfers.cpp
@@ -7,57 +7,29 @@
 
 using namespace std;
 
-unordered_map<string, int> name_to_id;
-int next_id = 1;
-
-#define MAXN 10000
-int nadj[MAXN];
-int adj[MAXN][MAXN];
-
-const int COM = 1;
-
-int count_table[MAXN];
-int level[MAXN];
-int parent[MAXN];
-
-// count direct and indirect orbits from v
-int count(int v) {
-  auto &c = count_table[v];
-  if (c != -1) {
-    return c;
+template <typename MapType>
+typename MapType::mapped_type *lookup(MapType &m,
+                                      const typename MapType::key_type &k) {
+  auto it = m.find(k);
+  if (it != m.end()) {
+    return &it->second;
   }
-
-  int sum = 0;
-  for (int i = 0; i < nadj[v]; i++) {
-    sum += 1 + count(adj[v][i]);
-  }
-
-  c = sum;
-  return c;
+  return nullptr;
 }
 
-void calculate_parents() {
-  memset(parent, -1, sizeof(parent));
+unordered_map<string, string> parent;
 
-  queue<int> q;
-  q.push(COM);
-  parent[COM] = -1;
-
-  while (!q.empty()) {
-    int v = q.front();
-    q.pop();
-
-    for (int i = 0; i < nadj[v]; i++) {
-      int w = adj[v][i];
-      parent[w] = v;
-      q.push(w);
-    }
+int count(const string &k) {
+  if (const auto *p = lookup(parent, k)) {
+    return 1 + count(*p);
   }
+  return 0;
 }
 
-int dist_to_santa(int from, int santa) {
-  for (int par = santa, par_dist = 0; par >= 0; par = parent[par], par_dist++) {
-    if (par == from) {
+int dist_to_dest(const string &from, string dest) {
+  string *par = &dest;
+  for (int par_dist = 0; par; par = lookup(parent, *par), par_dist++) {
+    if (*par == from) {
       return par_dist;
     }
   }
@@ -65,47 +37,28 @@ int dist_to_santa(int from, int santa) {
 }
 
 int main() {
-  name_to_id["COM"] = COM;
-  next_id = COM + 1;
-  memset(nadj, 0, sizeof(nadj));
-
   char a[5];
   char b[5];
   while (scanf("%3s)%3s\n", a, b) == 2) {
-    int id_a = name_to_id[a];
-    int id_b = name_to_id[b];
-    if (id_a == 0) {
-      id_a = next_id++;
-      name_to_id[a] = id_a;
-    }
-    if (id_b == 0) {
-      id_b = next_id++;
-      name_to_id[b] = id_b;
-    }
-
-    adj[id_a][nadj[id_a]++] = id_b;
+    parent[b] = a;
   }
 
-  // number of orbits and indirect orbits (part 1)
-  memset(count_table, -1, sizeof(count_table));
-  int sum = 0;
-  for (int i = 1; i < next_id; i++) {
-    sum += count(i);
+  int count_sum = 0;
+  for (const auto &p : parent) {
+    count_sum += count(p.first);
   }
-  printf("count: %d\n", sum);
+  printf("count: %d\n", count_sum);
 
   // minimum num of transfers (part 2)
-  int YOU = name_to_id["YOU"];
-  int SAN = name_to_id["SAN"];
-  calculate_parents();
   int min_dist = numeric_limits<int>::max();
-  for (int par = parent[YOU], par_dist = 1; par >= 0;
-       par = parent[par], par_dist++) {
-    int dist = dist_to_santa(par, SAN);
+  int par_dist = 1;
+  for (auto *par = lookup(parent, "YOU"); par; par = lookup(parent, *par)) {
+    int dist = dist_to_dest(*par, "SAN");
     if (dist >= 0) {
       dist += par_dist;
       min_dist = min(min_dist, dist);
     }
+    par_dist++;
   }
   printf("min_transfers: %d\n", min_dist - 2);
 

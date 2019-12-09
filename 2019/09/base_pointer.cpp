14079eedf5e53ce96cc2344076045f2362e88a14
Felipe Oliveira Carvalho <felipekde@gmail.com>
2019-12-09 10:29:17 +0100

    Day 09

---
+++ 2019/09/base_pointer.cpp
@@ -0,0 +1,25 @@
+#include <cstdio>
+#include <vector>
+
+#include "intcode.h"
+
+int main() {
+  std::vector<long long> data;
+  long long code;
+  while (scanf("%lld", &code)) {
+    data.push_back(code);
+    char c = getchar();
+    if (c == EOF || c == '\n') {
+      break;
+    } else {
+      assert(c == ',');
+    }
+  }
+
+  VM vm(std::move(data));
+  vm.pushInput(1); // tests
+  // vm.pushInput(2);  // production
+  vm.run();
+
+  return 0;
+}

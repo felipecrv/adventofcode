1a46251116bdb38944d2b7ada9862e4bff13878e
Felipe Oliveira Carvalho <felipekde@gmail.com>
2021-12-11 14:17:11 +0100

    Clean day 07 solution a bit

--- 2021/07/crab.cpp
+++ 2021/07/crab.cpp
@@ -5,8 +5,8 @@
 
 using namespace std;
 
-long long fuel(const vector<int> &initial_pos, bool linear_consumption,
-               int target_pos) {
+long long costToMoveAllCrabs(const vector<int> &initial_pos,
+                             bool linear_consumption, int target_pos) {
   long long cost = 0;
   for (auto pos : initial_pos) {
     const long long dist = abs(target_pos - pos);
@@ -15,12 +15,15 @@ long long fuel(const vector<int> &initial_pos, bool linear_consumption,
   return cost;
 }
 
-void solve(const vector<int> &initial_pos, bool linear_consumption,
-           int max_pos) {
+void solve(
+    const vector<int> &initial_pos,  // initial pos of each crab
+    bool linear_consumption,     // 1 cost per space unit (liner) or range sum
+    int min_pos, int max_pos) {  // range of possible solutions
   int best_pos = -1;
   long long min_cost = numeric_limits<long long>::max();
-  for (int target_pos = 0; target_pos <= max_pos; target_pos++) {
-    long long cost = fuel(initial_pos, linear_consumption, target_pos);
+  for (int target_pos = min_pos; target_pos <= max_pos; target_pos++) {
+    long long cost =
+        costToMoveAllCrabs(initial_pos, linear_consumption, target_pos);
     if (cost < min_cost) {
       best_pos = target_pos;
       min_cost = cost;
@@ -31,20 +34,22 @@ void solve(const vector<int> &initial_pos, bool linear_consumption,
 
 int main() {
   vector<int> initial_pos;
+  int min_pos = 0;
   int max_pos = 0;
   int p;
   while (scanf("%d", &p) == 1) {
     char c = getchar();
     assert(c == ',' || c == '\n' || c == EOF);
     initial_pos.push_back(p);
+    min_pos = min(min_pos, p);
     max_pos = max(max_pos, p);
   }
-  printf("crabs around [0, %d]\n", max_pos);
+  printf("crabs are distributed in the [%d, %d] range\n", min_pos, max_pos);
 
   puts("part 1:");
-  solve(initial_pos, true, max_pos);
+  solve(initial_pos, true, min_pos, max_pos);
   puts("part 2:");
-  solve(initial_pos, false, max_pos);
+  solve(initial_pos, false, min_pos, max_pos);
 
   return 0;
 }
